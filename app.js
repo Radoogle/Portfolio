d68ff0f3af398c9f22384df410a24dd4d147459b
Radu H <52208792+Radoogle@users.noreply.github.com>
2021-03-01 17:47:27 +0200

    read from file

--- app.js
+++ app.js
@@ -1,164 +1,5 @@
 // jshint esversion:6
 
-const PROJECTS = [ 
-    {
-        name: 'eCommerce Website',
-        link: 'https://radoogle.github.io/redStore/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/eCommerce.jpg',
-        category: ['All']
-    },
-    {
-        name: 'Chat App Page',
-        link: 'https://radoogle.github.io/chat-app/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/chatAppPage.jpg',
-        category: ['All', 'codeberry']
-    },
-    {
-        name: 'Sign Up Page',
-        link: 'https://radoogle.github.io/signup-page/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/signUpPage.jpg',
-        category: ['All', 'codeberry']
-    },
-    {
-        name: 'Contact Me Page',
-        link: 'https://radoogle.github.io/contact-me-page/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/contactMePage.jpg',
-        category: ['All', 'codeberry']
-    },
-    {
-        name: 'Blog Main Page',
-        link: 'https://radoogle.github.io/blog-main-page/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/blogPage.jpg',
-        category: ['All', 'codeberry']
-    },
-    {
-        name: 'Tic Tac Toe',
-        link: 'https://radoogle.github.io/Tic-Tac-Toe/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/TicTacToe.jpg',
-        category: ['All', 'game']
-    },
-    {
-        name: 'Tree Map (D3)',
-        link: 'https://codepen.io/Radoogle/full/GRpPzVG',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/D3TreeMap.jpg',
-        category: ['All', 'D3', 'CodePen', 'fcc']
-    },
-    {
-        name: 'Choropleth Map (D3)',
-        link: 'https://codepen.io/Radoogle/full/xxwmWoz?',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/ChoroplethMap.jpg',
-        category: ['All', 'D3', 'CodePen', 'fcc']
-    },
-    {
-        name: 'Pomodoro Clock (React)',
-        link: 'https://codepen.io/Radoogle/pen/xxwYrjL?editors=0010',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/PomodoroClock.jpg',
-        category: ['All', 'React', 'CodePen', 'fcc']
-    },
-    {
-        name: 'Heat Map (D3)',
-        link: 'https://codepen.io/Radoogle/pen/mdejBQb?editors=0010',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/HeatMap.jpg',
-        category: ['All', 'D3', 'CodePen', 'fcc']
-    },
-    {
-        name: 'Scatter Plot (D3)',
-        link: 'https://codepen.io/Radoogle/pen/VwvXoqR?editors=0010',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/D3ScatterPlot.jpg',
-        category: ['All', 'D3', 'CodePen', 'fcc']
-    },
-    {
-        name: 'Calculator (React)',
-        link: 'https://codepen.io/Radoogle/pen/mdewwgm?editors=0010',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/Calculator.jpg',
-        category: ['All', 'React', 'CodePen']
-    },
-    {
-        name: 'Drum Machine (React)',
-        link: 'https://codepen.io/Radoogle/pen/XWmMKLK',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/drummachine.jpg',
-        category: ['All', 'React', 'CodePen']
-        
-    },
-    {
-        name: 'Memory Game',
-        link: 'https://radoogle.github.io/Memory-Game/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/MemoryGame.jpg',
-        category: ['All', 'game']
-        
-    },
-    {
-        name: 'Bar Chart (D3)',
-        link: 'https://codepen.io/Radoogle/pen/BaowWqx?editors=0010',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/D3BarChart.jpg',
-        category: ['All', 'D3', 'CodePen']
-        
-    },
-    {
-        name: 'Tribute Page',
-        link: 'https://codepen.io/Radoogle/pen/GRJvMGy',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/tribute.jpg',
-        category: ['All', 'fcc', 'CodePen']
-        
-    },
-    {
-        name: 'Survey Form',
-        link: 'https://codepen.io/Radoogle/pen/mdJMXMw',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/survey.jpg',
-        category: ['All', 'fcc', 'CodePen']
-        
-    },
-    {
-        name: 'Product Landing Page',
-        link: 'https://codepen.io/Radoogle/pen/ExjvBZN',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/product.jpg',
-        category: ['All', 'fcc', 'CodePen']
-        
-    },
-    {
-        name: 'Technical Documentation Page',
-        link: 'https://codepen.io/Radoogle/pen/yLNzKBW',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/documentation.jpg',
-        category: ['All', 'fcc', 'CodePen']
-        
-    },
-    {
-        name: 'Countdown Timer',
-        link: 'https://radoogle.github.io/Countdown-timer/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/countdown.jpg',
-        category: ['All', 'github']
-        
-    },
-    {
-        name: 'Image Manipulation',
-        link: 'https://radoogle.github.io/ImageManipulation/',
-        className: 'tiles',
-        imgSrc: 'images/project-thumbnails/manipulation.jpg',
-        category: ['All', 'github']
-        
-    },
-];
-
 const displayProjects = (projects, filterValue) => {
   let result = "";
   
@@ -203,9 +44,15 @@ function scrollDownFunction() {
   window.scrollTo(0,document.querySelector("#welcome-section").scrollHeight + 75);
 }
 
-
 document.addEventListener('DOMContentLoaded', function() {
-    const filter = document.getElementById("filter");
+const filter = document.getElementById("filter");
+
+// readFromJSONFile();
+fetch('projects-db.json')
+  .then(response => response.json())
+  .then(PROJECTS => 
+    {
+    
 
     // first display all project, category === "All"
     displayProjects(PROJECTS, "All"); 
@@ -230,12 +77,13 @@ document.addEventListener('DOMContentLoaded', function() {
         `;
       });
       filter.innerHTML = resultList;
-      
+    
     // every change in filter list will refresh the list of tiles.
     filter.addEventListener("change", () => {
       displayProjects(PROJECTS, filter.value);
     });
 
+  });
     const footerYear = document.getElementById("footerYear");
     footerYear.innerHTML = new Date().getFullYear();
 });
