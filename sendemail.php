a436ba608dc4d7e1c7e652b2f075d8254fdf5051
Radu H <52208792+Radoogle@users.noreply.github.com>
2021-03-01 11:39:21 +0200

    php test

--- sendemail.php
+++ sendemail.php
@@ -1,21 +1,19 @@
 <?php
 
-  $to  = 'radu.harangus@gmail.com';
-  $name = $_POST['name'];
+  $to            = 'radu.harangus@gmail.com';
+  $name          = $_POST['name'];
   $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
+  $message       = $_POST['message'];
   $email_subject = "email sent from the contact page (portfolio)";
 
-  $email_body = "You have received a new message. ".
-                " Here are the details:\n Name: $name \n ".
-                "Email: $email_address\n Message \n $message";
-
+  $email_body    = "You have received a new message. Here are the details:\n 
+                    Name: $name \n
+                    Email: $visitor_email \n 
+                    Message: \n 
+                    $message";
 
   echo '<script>alert("Welcome to Geeks for Geeks")</script>'; 
   
-
-if ($_POST["message"]) {
-    mail($to, $email_subject, $email_body. "From: $visitor_email");
-}
+  mail($to, $email_subject, $email_body);
 
 ?>
